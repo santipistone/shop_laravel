cda34ab249cabfddae023f56d08b666ac0cf9658
santipistone <80516520+santipistone@users.noreply.github.com>
2021-06-11 13:19:57 +0200

    Add files via upload

---
+++ resources/views/error_item.blade.php
@@ -0,0 +1,4 @@
+<div class="unbd2" >
+<p>Prodotto non esistente </p>
+</div>
+<meta http-equiv="refresh" content="4; URL='/home" />
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,8 +1,8 @@
 <div id="box4">
 <form action="{{ action('Login@checkLogin') }}" method="POST">
 <input type="hidden" name="_token" value="{{ $csrf_token }}">
-<input class="input-box2" name="email" placeholder="Username">
-<input type="password" name="passw" id="change-id" class="input-box2" placeholder="Password">
+<input required="" class="input-box2" name="email" placeholder="Username">
+<input required="" type="password" name="passw" id="change-id" class="input-box2" placeholder="Password">
 <img src="{{ URL::asset('img/eye-slash.png') }}" class="ico-eye"></img>
 <input id="but6" value="Accedi" type="submit"><br>
 <a href="{{ URL('home/reg') }}" id="but6">Registrati</a>
